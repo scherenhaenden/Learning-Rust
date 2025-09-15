47358b6adce7f5dd31ec031008e8adac9dbe9a33
scherenhaenden <scherenhaenden@gmail.com>
2025-09-15 14:24:36 +0200

    Add initial project structure for Rust application projects, including Cargo.toml and main.rs files for personal greeter and math quiz projects.

---
+++ language-projects/easy/project-02-math-quiz/src/main.rs
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+fn main() {
+
+    // inputs
+    println!("Enter a first number: ");
+    let mut first_number = String::new();
+    std::io::stdin().read_line(&mut first_number).unwrap();
+    let first_number: i32 = first_number.trim().parse().unwrap();
+
+    println!("what kind of operation do you want to perform? (+, -, *, /): ");
+    let mut operation = String::new();
+    std::io::stdin().read_line(&mut operation).unwrap();
+    let operation = operation.trim();
+
+    println!("Enter a second number: ");
+    let mut second_number = String::new();
+    std::io::stdin().read_line(&mut second_number).unwrap();
+    let second_number: i32 = second_number.trim().parse().unwrap();
+
+    // validation and floating point division
+    let result = match operation {
+        "+" => first_number + second_number,
+        "-" => first_number - second_number,
+        "*" => first_number * second_number,
+        "/" => first_number / second_number,
+        _ => {
+            println!("Invalid operation");
+            return;
+        }
+    };
+
+    // output
+    println!("The result is: {} {} {} = {}", first_number, operation, second_number, result);
+}
